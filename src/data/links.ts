5639f0e5b016c8cce82159b6b42c530cc2bb9e98
Wildan Fauzy <nadliwis@gmail.com>
2025-01-30 12:41:50 +0700

    Update links.ts

--- src/data/links.ts
+++ src/data/links.ts
@@ -5,13 +5,13 @@ import GithubIcon from '@/components/icons/GithubIcon'
 export const SOCIALNETWORKS = [
 	{
 		name: 'Github',
-		url: 'https://github.com/danielcgilibert/blog-template',
+		url: 'https://github.com/wilfauzy',
 		icon: GithubIcon
 	},
 
 	{
 		name: 'Twitter',
-		url: 'https://github.com/danielcgilibert/blog-template',
+		url: 'https://x.com/ponkpek',
 		icon: TwitterIcon
 	}
 ] as const
